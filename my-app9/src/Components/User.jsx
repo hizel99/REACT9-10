f8f67f721661c468f89cea024884709fed4fc943
hizel99 <bhizel84@gmail.com>
2022-12-09 15:13:12 -0600

    commit

--- my-app9/src/Components/User.jsx
+++ my-app9/src/Components/User.jsx
@@ -1,27 +1,26 @@
-import React , {useState} from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const User = () => {
-    const {id}=useParams()
-    console.log(id)
-    const [equipos, setEquipos] = useState([]);
-    React.useEffect(() => {
-        const getDates = async () => {
-            const Date = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-             const users=  await Date.json()
-             setEquipos(users)
-           
-           
-          };
-      getDates();
-    }, [id]);
-  
-    
-    
-    return ( <div>
-        <h3>{equipos.name}</h3>
-        <p>{equipos.email}</p>
-    </div> );
-}
- 
+  const { id } = useParams();
+  console.log(id);
+  const [equipos, setEquipos] = useState([]);
+  React.useEffect(() => {
+    const getDates = async () => {
+      const Date = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`
+      );
+      const users = await Date.json();
+      setEquipos(users);
+    };
+    getDates();
+  }, [id]);
+
+  return (
+    <div>
+      <h3>{equipos.name}</h3>
+      <p>{equipos.email}</p>
+    </div>
+  );
+};
+
 export default User;
